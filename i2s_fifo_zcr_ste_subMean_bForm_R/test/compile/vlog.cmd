e57f354b03224e8d4d5511bb5da3233387ff82e8
Zizhen Liu <liuzizhen1234@gmail.com>
2017-04-08 20:18:45 -0500

    roughness first draft done, not tested

---
+++ i2s_fifo_zcr_ste_subMean_bForm_R/test/compile/vlog.cmd
@@ -0,0 +1,1 @@
+-o D:/UIUC/2017_Spring/ECE_396/beamforming/test/compile -lc D:/UIUC/2017_Spring/ECE_396/beamforming/test/library.cfg -fl D:/UIUC/2017_Spring/ECE_396/beamforming/test/projlib.cfg -pli "C:/lscc/diamond/3.8_x64/active-hdl/bin/systf.dll" -epr D:/UIUC/2017_Spring/ECE_396/beamforming/test/compile/test.epr -dbg -work work D:/UIUC/2017_Spring/ECE_396/beamforming/mult16_array.v 
